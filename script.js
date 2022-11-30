c5ba5a28f9fad9d336614e7e9b9b29c13143028f
Ismael <Ismael.caraballo85@gmail.com>
2022-11-29 23:52:54 -0400

    menu

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 alert("     CAPTACION DE SOCIOS \nDEL CLUB 'ORIENTE PETROLERO'")
 let nombre= ""
 nombre = prompt("INGRESE SU NOMBRE POR FAVOR")
-alert("BIENVENIDO "+(nombre) +"POR FAVOR SELECCIONA LAS OPCIONES CORRECTAMENTE")
+alert("BIENVENIDO " +(nombre) +" POR FAVOR SELECCIONA LAS OPCIONES CORRECTAMENTE")
+
+let option = 0;
+do {
+    option = Number(prompt(" 1.- Registro de NUEVO SOCIO \n 2.- PAGO DE APORTES \n 3.- VER CATEGORIAS \n 0.- SALIR"  ))
+
+    
+} while (condition);
