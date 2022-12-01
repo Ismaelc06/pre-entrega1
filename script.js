8a2b88aff3b2f92361c0262a69cc296752c07db7
Ismael <Ismael.caraballo85@gmail.com>
2022-11-30 21:53:17 -0400

    act.

--- script.js
+++ script.js
@@ -13,40 +13,113 @@ do {
         switch (pointer) {
             case 1:
                 pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $300 \n 2.- SEMESTRAL $170")
-                if (pay===1) {
+                if (pay === 1) {
                     alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
-                } else if(pay===2){
-                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")  
-                }else{
-                    alert("Ingrese una opcion correcta por favor")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
                 }
                 break
             case 2:
                 pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $200 \n 2.- SEMESTRAL $120")
                 alert("Gracias por hacerte SOCIO DIAMANTE")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
                 break
             case 3:
                 pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $150 \n 2.- SEMESTRAL $80")
                 alert("Gracias por hacerte SOCIO PLATINO")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
                 break
             case 4:
                 pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $100 \n 2.- SEMESTRAL $60")
                 alert("Gracias por hacerte SOCIO GENERAL")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
                 break
             case 5:
                 pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $70 \n 2.- SEMESTRAL $50")
                 alert("Gracias por hacerte SOCIO CURVA")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
                 break
 
             default:
+                alert("Por favor ingrese una opcion correcta")
                 break
         }
 
-    } else {
+    } else if (option === 2) {
+        pointer = Number(prompt("Seleccione su categoria \n 1.- Benefactor \n 2.- Diamante \n 3.- Platino \n 4.- General \n 5.- Curva \n 0.- SALIR"))
 
+        switch (pointer) {
+            case 1:
+                pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $300 \n 2.- SEMESTRAL $170")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
+                break
+            case 2:
+                pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $200 \n 2.- SEMESTRAL $120")
+                alert("Gracias por hacerte SOCIO DIAMANTE")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
+                break
+            case 3:
+                pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $150 \n 2.- SEMESTRAL $80")
+                alert("Gracias por hacerte SOCIO PLATINO")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
+                break
+            case 4:
+                pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $100 \n 2.- SEMESTRAL $60")
+                alert("Gracias por hacerte SOCIO GENERAL")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
+                break
+            case 5:
+                pay = prompt("SELECCIONE METODO DE PAGO \n 1.-ANUAL $70 \n 2.- SEMESTRAL $50")
+                alert("Gracias por hacerte SOCIO CURVA")
+                if (pay === 1) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                } else if (pay === 2) {
+                    alert("Gracias por hacerte SOCIO BENEFACTOR SEMESTRAL")
+                }
+                break
+
+            default:
+                alert("Por favor ingrese una opcion correcta")
+                break
+        }
+    
+    }else if (option==3){
+        alert("Las categorias disponibles son: \n 1.- Benefactor \n 2.- Diamante \n 3.- Platino \n 4.- General \n 5.- Curva")
 
     }
 
 
-} while (option!=0)
-  alert("Por favor ingresa una opcion correcta")
+} while (option != 0)
+alert("Por favor ingresa una opcion correcta")
